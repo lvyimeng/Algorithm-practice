fa84f0065e9fbd0c0973781091283ed013e18279
Yimeng <lvyimeng@users.noreply.github.com>
2013-10-04 21:20:12 -0400

    Update simplesort.java

--- simplesort.java
+++ simplesort.java
@@ -1,3 +1,5 @@
+public Class simplesort{
+
 public static void bubblesort(int[] arr){
   for(int i=0;i<arr.length;i++){
     for(int j=arr.length-1;j>i;j--){
@@ -25,4 +27,6 @@ public static void selectionsort(int[] arr){
       arr[min_index]=temp;
     }
   }
+}
+
 }
