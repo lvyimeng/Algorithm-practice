66fd749cc93c6a8839b3bb2db7d730a9222b4c44
Yimeng <lvyimeng@users.noreply.github.com>
2013-10-04 21:24:19 -0400

    Update Brute-Force.java

--- Brute-Force.java
+++ Brute-Force.java
@@ -1,14 +1,18 @@
+public Class Brute-Force{
+	
 public static int BruteForce(String source,String sub){
-	  int source_index=0,sub_index=0,index=-1;
-	  while((source_index<source.length())&&(sub_index<sub.length())){
-	     if(source.charAt(source_index)==sub.charAt(sub_index)){
-	       source_index++;
-	       sub_index++;
-	     }else{
-	       source_index=source_index-sub_index+1;
-	       sub_index=0;
-	     }
-	     if(sub_index==sub.length()) return source_index-sub_index;
-	  }
-	  return -1;
+  int source_index=0,sub_index=0,index=-1;
+  while((source_index<source.length())&&(sub_index<sub.length())){
+    if(source.charAt(source_index)==sub.charAt(sub_index)){
+      source_index++;
+      sub_index++;
+    }else{
+      source_index=source_index-sub_index+1;
+      sub_index=0;
+    }
+    if(sub_index==sub.length()) return source_index-sub_index;
+  }
+  return -1;
+}
+
 }
