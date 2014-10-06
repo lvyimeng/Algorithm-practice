058924d2a038e143abc382ab7a65248cc1b0cba9
Yimeng <lvyimeng@users.noreply.github.com>
2014-10-06 12:57:53 -0400

    Create LongestPalindromicSubstringDP.java

---
+++ LongestPalindromicSubstringDP.java
@@ -0,0 +1,53 @@
+string longestPalindromeDP(string s) {
+  int n = s.length();
+  int longestBegin = 0;
+  int maxLen = 1;
+  bool table[1000][1000] = {false};
+  for (int i = 0; i < n; i++) {
+    table[i][i] = true;
+  }
+  for (int i = 0; i < n-1; i++) {
+    if (s[i] == s[i+1]) {
+      table[i][i+1] = true;
+      longestBegin = i;
+      maxLen = 2;
+    }
+  }
+  for (int len = 3; len <= n; len++) {
+    for (int i = 0; i < n-len+1; i++) {
+      int j = i+len-1;
+      if (s[i] == s[j] && table[i+1][j-1]) {
+        table[i][j] = true;
+        longestBegin = i;
+        maxLen = len;
+      }
+    }
+  }
+  return s.substr(longestBegin, maxLen);
+}
+
+string expandAroundCenter(string s, int c1, int c2) {
+  int l = c1, r = c2;
+  int n = s.length();
+  while (l >= 0 && r <= n-1 && s[l] == s[r]) {
+    l--;
+    r++;
+  }
+  return s.substr(l+1, r-l-1);
+}
+ 
+string longestPalindromeSimple(string s) {
+  int n = s.length();
+  if (n == 0) return "";
+  string longest = s.substr(0, 1);  // a single char itself is a palindrome
+  for (int i = 0; i < n-1; i++) {
+    string p1 = expandAroundCenter(s, i, i);
+    if (p1.length() > longest.length())
+      longest = p1;
+ 
+    string p2 = expandAroundCenter(s, i, i+1);
+    if (p2.length() > longest.length())
+      longest = p2;
+  }
+  return longest;
+}
